7b5fbc49b31c61f3cb561bd0b0d3ab1adf6a0b3e
kgabriel-dev <gabrielkleebaum@yahoo.de>
2025-12-17 20:28:40 +0100

    improve split_number algorithm to use placeholders

--- sumplete.py
+++ sumplete.py
@@ -1,11 +1,21 @@
 import random
 
 
-def split_number(number: int, count: int, parts: list[int]) -> list[int]:
+def split_number(number: int, count: int, parts_input: list[int]) -> list[int]:
+    placeholder = -1
+
+    parts = []
+    # collect all valid parts; break at the first placeholder
+    for part in parts_input:
+        if part != placeholder:
+            parts.append(part)
+        else:
+            break
+
     remaining_number = number - sum(parts)
     remaining_count = count - len(parts)
     
-    if len(parts) == count or remaining_number == 0:
+    if remaining_count == 0 or remaining_number == 0:
         return parts
 
     # if only 1 more umber is needed, add the remaining number and return
@@ -15,10 +25,18 @@ def split_number(number: int, count: int, parts: list[int]) -> list[int]:
     
     # calculate a new number
     # ensure at least 1 is left for each remaining part
-    new_part = random.randrange(1, 1 + remaining_number - (remaining_count - 1))
+    new_part = random.randint(1, remaining_number - (remaining_count - 1))
     parts.append(new_part)
 
-    return split_number(number, count, parts)
+    # fill in the parts list and fill placeholders
+    parts_result = []
+    for i in range(count):
+        if i < len(parts):
+            parts_result.append(parts[i])
+        else:
+            parts_result.append(placeholder)
+
+    return split_number(number, count, parts_result)
 
 
 def __main__():
